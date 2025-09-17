b97dac0e9b2335b381c5598dfcca0750d121d0ab
Gokul-Oak-Coder <gokuloakcoder@gmail.com>
2025-09-17 16:24:58 +0530

    fix: 1.added new duration formatter in the song_helper class.

--- lib/helper/song_helper.dart
+++ lib/helper/song_helper.dart
@@ -1,8 +1,12 @@
 class SongHelper {
-  static String formatDuration(Duration duration) {
+  static String formatDuration1(Duration duration) {
     String twoDigits(int n) => n.toString().padLeft(2, '0');
     final minutes = twoDigits(duration.inMinutes.remainder(60));
     final seconds = twoDigits(duration.inSeconds.remainder(60));
     return "$minutes:$seconds";
+  }
+
+  static String formatDuration(Duration duration) {
+    return duration.toString().split('.').first.padLeft(8, "0");
   }
 }
